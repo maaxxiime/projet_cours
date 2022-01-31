d3ed9cd02ac9730a1a7feeec8cdc9a3a6df60d17
cynthiaS7 <97170111+cynthiaS7@users.noreply.github.com>
2022-01-31 15:04:40 +0100

    ccc

---
+++ assets/js/pages_deux.js
@@ -0,0 +1,7 @@
+function myCard() {
+    let tableau = document.getElementById('table').value;
+    let ligne = document.getElementById('tr').value;
+    let mots = document.getElementById('td').value;
+    let nom = document.getElementById('nom').value;
+    let name = document.getElementById('name').value;
+}
