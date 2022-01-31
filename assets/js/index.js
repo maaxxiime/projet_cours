b1fa2603f52876f4355218e3e9b9130320ee2362
Maxime <maximeringeval@orange.fr>
2022-01-31 15:10:38 +0100

    darkmode

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,9 +30,43 @@ function add_3() {
     } else {
         document.getElementById("liste").appendChild(list);
         liste.style.display = "block"
-        document.getElementById("inpt").value = "";
+    }
+    document.getElementById("inpt").value = "";
+
+
+
+    let span = document.createElement('span');
+    let dtxt = document.createTextNode('\u00D7');
+    span.className = 'del';
+    span.appendChild(dtxt);
+    list.appendChild(span);
+
+    for (let i = 0; i < del.length; i++) {
+        del[i].onclick = function() {
+            let div = this.parentElement;
+            div.style.display = 'none';
+        }
     }
 }
+let myNodelist = document.getElementsByTagName('LI');
+let i;
+for (let i = 0; i < myNodelist.length; i++) {
+    let span = document.createElement("span");
+    let dtxt = document.createTextNode("\u00D7");
+    span.className = 'del';
+    span.appendChild(dtxt);
+    myNodelist[i].appendChild(span);
+}
+
+let del = document.getElementsByClassName('del');
+for (i = 0; i < del.length; i++) {
+    del[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+        console.log(div);
+    }
+}
+
 
 
 
@@ -47,3 +81,9 @@ function delete_3() {
     window.location.reload();
 }
 l.onclick = function(){delete_3()};
+
+
+function dark() {
+    let element = document.body;
+    element.classList.toggle("dark");
+}
