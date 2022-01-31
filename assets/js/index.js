620afe7f96e6881e87ecdcaa40b3caa9a0c0b825
Maxime <maximeringeval@orange.fr>
2022-01-31 11:54:16 +0100

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,35 @@ function connexion() {
         alert("identifiant incorrect");
     } 
 }
+
+
+
+function add_3() {
+    let texte = document.getElementById("inpt").value;
+    let list = document.createElement("li");
+    let text = document.createTextNode(texte);
+    list.appendChild(text);
+
+    if (texte === "") {
+        alert("champ vide");
+
+    } else {
+        document.getElementById("liste").appendChild(list);
+        liste.style.display = "block"
+        document.getElementById("inpt").value = "";
+    }
+}
+
+
+
+function deconnexion() {
+    alert("vous avez été déconnectée");
+    window.location.href = '/index.html'; //fait le lien vers une nouvelle page
+}
+
+
+
+function delete_3() {
+    window.location.reload();
+}
+l.onclick = function(){delete_3()};
