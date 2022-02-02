64fe889b71bd610ace3ba6c868c34373010b886e
cynthiaS7 <97170111+cynthiaS7@users.noreply.github.com>
2022-02-02 12:11:01 +0100

    dd

--- assets/js/pages_deux.js
+++ assets/js/pages_deux.js
@@ -1,7 +1,29 @@
 function myCard() {
-    let tableau = document.getElementById('table').value;
-    let ligne = document.getElementById('tr').value;
-    let mots = document.getElementById('td').value;
-    let nom = document.getElementById('nom').value;
-    let name = document.getElementById('name').value;
+    // let table = document.getElementById("tbe").value;
+    let input = document.getElementById('nom').value;
+    let td = document.createElement('td');
+    let zone = document.createTextNode(input);
+    td.appendChild(zone);
+
+    if( input === ""){
+        alert("Veuillez remplir le champs")
+    }else{
+        alert("Vos Informations ont bien été enregistrée")
+        document.getElementById('tbe').appendChild(td);
+        console.log(td);
+    }
+    
+    document.getElementById("nom").value = '';
+    let span = document.createElement('span');
+    let txt = document.createTextNode('/u00D7');
+    span.appendChild(txt);
+    td.appendChild(span);
+
+    for (i = 0; i < del; i++ ) {
+        del[i].onclick = function() {
+            let div = this.parentElement;
+            div.style.display = 'none';
+        }
+    }
+    
 }
