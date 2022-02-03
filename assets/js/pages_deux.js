8728e43ca1e414c6d9d888e6ae3ad706b5c9c6b6
cynthiaS7 <97170111+cynthiaS7@users.noreply.github.com>
2022-02-03 22:30:18 +0100

    un

--- assets/js/pages_deux.js
+++ assets/js/pages_deux.js
@@ -1,12 +1,16 @@
 function myCard() {
     // let table = document.getElementById("tbe").value;
     let input = document.getElementById('nom').value;
+    let inputt = document.getElementById('name').value;
+    let inputs = document.getElementById('date').value;
+    let inputtt = document.getElementById('sexe').value;
+    let inputss = document.getElementById('prescripteur').value;
     let td = document.createElement('td');
     let zone = document.createTextNode(input);
     td.appendChild(zone);
 
-    if( input === ""){
-        alert("Veuillez remplir le champs")
+    if( input === "" || inputt === "" || inputs === "" || inputtt === "" || inputss === ""){
+        alert("Veuillez remplir le champ")
     }else{
         alert("Vos Informations ont bien été enregistrée")
         document.getElementById('tbe').appendChild(td);
@@ -15,15 +19,45 @@ function myCard() {
     
     document.getElementById("nom").value = '';
     let span = document.createElement('span');
-    let txt = document.createTextNode('/u00D7');
-    span.appendChild(txt);
     td.appendChild(span);
 
-    for (i = 0; i < del; i++ ) {
+    document.getElementById("name").value = '';
+    let spann = document.createElement('span');
+    td.appendChild(spann);
+
+    document.getElementById("date").value = '';
+    let spaan = document.createElement('span');
+    td.appendChild(spaan);
+
+    document.getElementById("sexe").value = '';
+    let spane = document.createElement('span');
+    td.appendChild(spane);
+
+    document.getElementById("prescripteur").value = '';
+    let spa = document.createElement('span');
+    td.appendChild(spa);
+
+    for (i = 0; i < del.length; i++ ) {
         del[i].onclick = function() {
             let div = this.parentElement;
             div.style.display = 'none';
         }
+    } 
+}
+
+let global = document.getElementsByTagName('TD');
+
+for (let i = 0; i < global.length; i++) {
+    let span = document.createElement("span");
+    span.className = "del";
+    global[i].appendChild(span);
+}
+
+let del = document.getElementsByClassName("del");
+
+for (i = 0; i < del.length; i++) {
+    del[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = 'none';
     }
-    
 }
