d0337590cab79b02e588432d5d55fde71779cd1f
cynthiaS7 <97170111+cynthiaS7@users.noreply.github.com>
2022-02-04 11:54:51 +0100

    aaa

--- assets/js/pages_deux.js
+++ assets/js/pages_deux.js
@@ -1,20 +1,63 @@
 function myCard() {
-    // let table = document.getElementById("tbe").value;
     let input = document.getElementById('nom').value;
     let inputt = document.getElementById('name').value;
     let inputs = document.getElementById('date').value;
     let inputtt = document.getElementById('sexe').value;
     let inputss = document.getElementById('prescripteur').value;
     let td = document.createElement('td');
+    let tdd = document.createElement('td');
+    let ttd = document.createElement('td');
+    let ttdd = document.createElement('td');
+    let dd = document.createElement('td');
     let zone = document.createTextNode(input);
-    td.appendChild(zone);
+    let zon = document.createTextNode(inputt);
+    let zonn = document.createTextNode(inputs);
+    let zonne = document.createTextNode(inputtt);
+    let zozo = document.createTextNode(inputss);
+    let but = document.getElementById("button");
 
-    if( input === "" || inputt === "" || inputs === "" || inputtt === "" || inputss === ""){
+    td.appendChild(zone);
+    tdd.appendChild(zon);
+    ttd.appendChild(zonn);
+    ttdd.appendChild(zonne);
+    dd.appendChild(zozo);
+
+    if(input === ""){
         alert("Veuillez remplir le champ")
     }else{
         alert("Vos Informations ont bien été enregistrée")
         document.getElementById('tbe').appendChild(td);
-        console.log(td);
+        console.log(td)
+    }
+    
+    if(inputt === ""){
+        alert("Veuillez remplir le champ")
+    }else {
+        document.getElementById('tr').appendChild(tdd);
+        console.log(tdd);
+    }
+
+    if(inputs === ""){
+        alert("Veuillez remplir le champ")
+    }else{
+        document.getElementById('tr').appendChild(ttd);
+        console.log(ttd);
+    }
+
+    if(inputtt === ""){
+        alert("Veuillez remplir le champ")
+    }else{
+        document.getElementById('tr').appendChild(ttdd);
+        console.log(ttdd);
+    }
+
+    if(inputss === ""){
+        alert("Veuillez remplir le champ")
+    }else{
+        document.getElementById('tr').appendChild(dd);
+        console.log(dd);
+        but.style.display = "none";
+
     }
     
     document.getElementById("nom").value = '';
@@ -23,19 +66,19 @@ function myCard() {
 
     document.getElementById("name").value = '';
     let spann = document.createElement('span');
-    td.appendChild(spann);
+    tdd.appendChild(spann);
 
     document.getElementById("date").value = '';
     let spaan = document.createElement('span');
-    td.appendChild(spaan);
+    ttd.appendChild(spaan);
 
     document.getElementById("sexe").value = '';
     let spane = document.createElement('span');
-    td.appendChild(spane);
+    ttdd.appendChild(spane);
 
     document.getElementById("prescripteur").value = '';
     let spa = document.createElement('span');
-    td.appendChild(spa);
+    dd.appendChild(spa);
 
     for (i = 0; i < del.length; i++ ) {
         del[i].onclick = function() {
@@ -43,14 +86,7 @@ function myCard() {
             div.style.display = 'none';
         }
     } 
-}
-
-let global = document.getElementsByTagName('TD');
-
-for (let i = 0; i < global.length; i++) {
-    let span = document.createElement("span");
-    span.className = "del";
-    global[i].appendChild(span);
+    
 }
 
 let del = document.getElementsByClassName("del");
